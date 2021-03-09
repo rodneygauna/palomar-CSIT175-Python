4322e88bdf439700f9d7363ef26c2c5e742c7425
Rodney Gauna <rodneygauna@gmail.com>
2021-03-08 22:51:34 -0800

    redo of assignment 4 plus single file testing

--- 40/asgn4_module.py
+++ 40/asgn4_module.py
@@ -2,42 +2,20 @@
 This module if for Assignment 4.
 There are two functions; is_field_blank() obtains the user's first name, while is_field_a_number() obtains the user's age.
 """
-def is_field_blank():
+def is_field_blank(name):
     """
-    This function takes the user's first name.
-    If the user enters nothing, it will post an error and continue to prompt till something is entered.
+    Checks if the name (argument) is blank. If it not blank, it returns as True; else it will return as False
     """
-    while True:
-        global first_name
-        first_name = input("What is your first name? ")
+    if name.lower() != "":
+        return True
+    else:
+        return False
 
-        if first_name.lower() != "":
-            while True:
-                global last_name
-                last_name = input("\nWhat is your last name? ")
-
-                if last_name.lower() != "":
-                    return last_name
-                else:
-                    print("Last Name must be Entered")
-                    continue
-            return first_name
-        else:
-            print("First Name must be Entered\n")
-            continue
-
-def is_field_a_number():
+def is_field_a_number(number):
     """
-    This fuciton takes the user's age.
-    If the user enters nothing or the entry isn't a number (digit), it will post an error and continue to prompt till something is entered.
+    Checks if the age (argument) is blank. If it not blank, it returns as True; else it will return as False
     """
-    while True:
-        global user_age
-        age = input("\nWhat is your age? ")
-
-        if age.isdigit() == True:
-            user_age = int(age)
-            return user_age
-        else:
-            print("Age must be a Number")
-            continue
+    if number.isdigit() == True:
+        return True
+    else:
+        return False
--- 40/module_tester.py
+++ 40/module_tester.py
@@ -1,4 +1,4 @@
-import asgn4_module as func
+
 
 def main():
     print("Assignment 4\n")
@@ -7,17 +7,17 @@ def main():
         global first_name
         first_name = input("What is your first name? ")
 
-        if func.is_field_blank(first_name):
+        if is_field_blank(first_name):
             while True:
                 global last_name
                 last_name = input("\nWhat is your last name? ")
 
-                if func.is_field_blank(last_name):
+                if is_field_blank(last_name):
                     while True:
                         global user_age
                         age = input("\nWhat is your age? ")
 
-                        if func.is_field_a_number(age) == True:
+                        if is_field_a_number(age) == True:
                             user_age = int(age)
                             if user_age > 40:
                                 print("\nWell,", first_name, last_name, "it looks like you are over the hill")
@@ -36,6 +36,19 @@ def main():
         else:
             print("First Name must be Entered\n")
             continue
+
+
+def is_field_blank(name):
+    if name.lower() != "":
+        return True
+    else:
+        return False
+
+def is_field_a_number(number):
+    if number.isdigit() == True:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
