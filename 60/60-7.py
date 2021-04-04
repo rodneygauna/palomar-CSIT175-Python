ef25306ecdbc8799b1617b208eb039834a520101
Rodney Gauna <rodneygauna@gmail.com>
2021-04-04 13:18:22 -0700

    exercise 6.7

---
+++ 60/60-7.py
@@ -0,0 +1,16 @@
+# check if the filename exists
+while True:
+    # capture the the filename from the user
+    file_name = input("What file do you want to open? ")
+
+    try:
+        with open(file_name, "r") as file:
+            # if the filename exists, print out "Your file (file name) is there"
+            print(f"Your file {file_name} is there.")
+            break
+    # if the filename doesn't exist, explain it can't be found and have them try again
+    # use the FileNotFoundError exception
+    # also, add the error message from the exception
+    except FileNotFoundError as e:
+        print("Your file myfile.txt has the following error:", e)
+        continue
